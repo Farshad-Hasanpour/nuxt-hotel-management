8491e9d8838b0d69f173dbea517e250f77e41408
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2025-01-05 18:01:26 +0330

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-	<div>reservations</div>
+	<div>{{res}}</div>
 </template>
 
 <script setup lang="ts">
+const {$http} = useNuxtApp();
 
+const res = await useAsyncData('reservations', async () => {
+	return await $http(`/reservations`, {
+		method: 'GET',
+	});
+});
 </script>
